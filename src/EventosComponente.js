d496969fcbad73b120469f942de87aea8139dac5
Valido-101 <jesuszafra22@gmail.com>
2022-12-24 18:12:34 +0100

    Evento doble click

--- src/EventosComponente.js
+++ src/EventosComponente.js
@@ -6,11 +6,24 @@ export const EventosComponente = () => {
         alert(nombre + ", has dado click al botón");
     }
 
+    function handlerDoubleClick(e){
+        alert("Has dado doble click");
+    }
+
   return (
     <div>
         <h1>Eventos en React</h1>
-        {/*Evento click*/}
-        <button onClick={ e => handlerClick(e, "Jesús") }>Hola, haz click</button>
+
+        <p>
+            {/*Evento click*/}
+            <button onClick={ e => handlerClick(e, "Jesús") }>Hola, haz click</button>
+        </p>
+
+        <p>
+            {/*Evento doble click*/}
+            <button onDoubleClick={handlerDoubleClick}>Hola, haz doble click</button>
+        </p>
+        
     </div>
   )
 }
